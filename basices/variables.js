ed743b96df257884d4fd2d3437b370b0ce86dc94
kalapee <35555664+kalapee@users.noreply.github.com>
2023-11-14 16:41:26 +0000

    variables

---
+++ basices/variables.js
@@ -0,0 +1,14 @@
+const accountId = 141414
+let accountEmail = "kalapeegajjar@gmail.com"
+var accountPassword = "123456"
+accountCity = "Ahmedabad"
+
+//accountId = 121212 //
+accountEmail = "gajjarkalapee@gmail.com"
+accountPassword = "456789"
+accountCity = "gujrat"
+
+//console.log(accountId);//
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
