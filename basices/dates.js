592f5c346f7b10a97deaf86b7f93431e01b5f3c6
kalapee <35555664+kalapee@users.noreply.github.com>
2023-11-20 16:25:58 +0000

    Date and time in depth in javascript

---
+++ basices/dates.js
@@ -0,0 +1,15 @@
+// Dates
+
+//let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+
+//let myCreatedDate = new Date(2023, 0, 20, 5, 3)
+let myCreatedDate = new Date("11-20-2023")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
